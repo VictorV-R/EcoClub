7af16b7976288dec3374d65a37f096e5c291fd94
Victor Manuel <vvilcaro@unsa.edu.pe>
2022-11-22 20:04:47 -0500

    Agregando fragments al MainActivity de la aplicacion

--- app/src/main/java/com/example/ecoclub/MainActivity.java
+++ app/src/main/java/com/example/ecoclub/MainActivity.java
@@ -3,12 +3,43 @@ package com.example.ecoclub;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    HomeFragment homeFragment = new HomeFragment();
+    ComunityFragment comunityFragment = new ComunityFragment();
+    MapsFragment mapsFragment = new MapsFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+                        return true;
+                    case R.id.comunity:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, comunityFragment).commit();
+                        return true;
+                    case R.id.maps:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, mapsFragment).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
