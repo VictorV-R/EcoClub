4be37ddc2c51e124587578e320a7a1808fe4a8d5
walterHC <whuaracha@unsa.edu.pe>
2022-11-23 11:46:07 -0500

    tratando de entender el error del fragment login

--- app/src/main/java/com/example/ecoclub/fragments/LoginFragment.java
+++ app/src/main/java/com/example/ecoclub/fragments/LoginFragment.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.ecoclub.MainActivity;
 import com.example.ecoclub.R;
