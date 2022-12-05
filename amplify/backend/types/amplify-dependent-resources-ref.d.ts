4911bb952c5bf7ee9ed67e9225c6612cc04af6e5
AldairBryan <asalcedoch@unsa.edu.pe>
2022-12-04 20:39:45 -0500

    Archivos añadidos

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "ecoclub": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
