830c01cb43a8367d790ab9d2afe59ec36bab12c5
Luis Condori <lcondorivill@unsa.edu.pe>
2022-12-08 19:40:30 -0500

    Agregando diseño al BottomNavigationView

--- app/src/main/java/com/example/ecoclub/MainActivity.java
+++ app/src/main/java/com/example/ecoclub/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.ApplicationInfo;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
