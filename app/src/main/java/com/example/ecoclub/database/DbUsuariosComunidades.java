a0f27acf5fa1c0e2edd77a4d6498dc2b7d4ae484
AldairBryan <asalcedoch@unsa.edu.pe>
2022-12-21 16:23:42 -0500

    Se agrego clase DbUsuariosComunidades

---
+++ app/src/main/java/com/example/ecoclub/database/DbUsuariosComunidades.java
@@ -0,0 +1,21 @@
+package com.example.ecoclub.database;
+
+public class DbUsuariosComunidades extends DataBaseHelper{
+    public void insertarUsuarioComunidad(int id_usuario, int id_comunidad,String tipo_usuario,int id_rango){
+        String query= "INSERT INTO sys."+TABLE_USUARIOS_COMUNIDADES+" (id_usuario, id_comunidad,tipo_usuario,id_rango ) VALUES ('"+id_usuario+"' , '"+id_comunidad+"'" +
+                ", '"+tipo_usuario+"', '"+id_rango+"')";
+        ejecutarSentencia(query);
+    }
+
+    public void modificarUsuarioComunidad(int id_usuario,int id_comunidad,String tipo_usuario,int id_rango){
+        String query="UPDATE sys."+TABLE_USUARIOS_COMUNIDADES+" SET 'tipo_usuario' = '"+tipo_usuario+"' , 'id_rango'  = '"+id_rango+"'" +
+                "WHERE ('id_usuario' = '"+id_usuario+"') and ('id_comunidad' = '"+id_comunidad+"')";
+        ejecutarSentencia(query);
+    }
+
+    public void eliminarUsuarioComunidad (int id_usuario,int id_comunidad){
+        String query="DELETE FROM sys."+TABLE_USUARIOS_COMUNIDADES+" " +
+                "WHERE ('id_usuario' = '"+id_usuario+"') and ('id_comunidad' = '"+id_comunidad+"')";
+        ejecutarSentencia(query);
+    }
+}
