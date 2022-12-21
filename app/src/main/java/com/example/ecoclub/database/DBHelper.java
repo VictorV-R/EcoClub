316d18fc030182a6628aa676a130579fa2df13f0
AldairBryan <asalcedoch@unsa.edu.pe>
2022-12-20 21:25:57 -0500

    Se definieron las tablas (Strings)

--- app/src/main/java/com/example/ecoclub/database/DBHelper.java
+++ app/src/main/java/com/example/ecoclub/database/DBHelper.java
@@ -6,8 +6,15 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DBHelper extends SQLiteOpenHelper {
     private static final int DB_VERSION = 1;
-    private static final String DB_NAME = "database.db";
-    public static final String TABLE_USERS = "users";
+    private static final String DB_NAME = "database-nuna.db";
+
+    public static final String TABLE_USUARIOS = "Usuarios";
+    public static final String TABLE_COMUNIDADES = "Comunidades";
+    public static final String TABLE_LOGROS = "Logros";
+    public static final String TABLE_RANGOS = "Rangos";
+    public static final String TABLE_USUARIOS_COMUNIDADES = "Usuarios_comunidades";
+    public static final String TABLE_PARTICIPANTES_ACTIVIDADES = "Participantes_actividades";
+    public static final String TABLE_ACTIVIDADES = "Actividades";
 
     public DBHelper(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
@@ -15,20 +22,14 @@ public class DBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        String sql = "CREATE TABLE "+TABLE_USERS+"( user_id integer PRIMARY KEY AUTOINCREMENT NOT NULL, " +
-                "name text NOT NULL,"+
-                "lastName text NOT NULL,"+
-                "email text NOT NULL UNIQUE,"+
-                "phone text NOT NULL,"+
-                "password text NOT NULL)";
-
+        String sql = "";
         sqLiteDatabase.execSQL(sql);
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        sqLiteDatabase.execSQL("DROP TABLE if exists " + TABLE_USERS);
+        sqLiteDatabase.execSQL("" );
         onCreate(sqLiteDatabase);
     }
 }
