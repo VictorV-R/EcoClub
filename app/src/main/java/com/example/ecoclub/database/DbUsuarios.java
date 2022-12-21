4594d2058dba1c60f6a0947caee63fecbd675dbd
AldairBryan <asalcedoch@unsa.edu.pe>
2022-12-21 12:08:06 -0500

    Se agregaron clases Db usuarios y logros

--- app/src/main/java/com/example/ecoclub/database/DbUsuarios.java
+++ app/src/main/java/com/example/ecoclub/database/DbUsuarios.java
@@ -11,6 +11,20 @@ public class DbUsuarios extends DataBaseHelper{
         ejecutarSentencia(query);
     }
 
+    public void modificarUsuario(int id,String nombre, String contraseña){
+        String query="UPDATE sys."+TABLE_USUARIOS+" SET 'nombre' = '"+nombre+"' , 'contrasena'='"+contraseña+"' " +
+                "WHERE ('id_usuario' = '"+id+"')";
+        Log.d("INFO",query);
+        ejecutarSentencia(query);
+    }
+
+    public void eliminarUsuario(int id){
+        String query="DELETE FROM sys."+TABLE_USUARIOS+" " +
+                "WHERE ('id_usuario' = '"+id+"')";
+        Log.d("INFO",query);
+        ejecutarSentencia(query);
+    }
+
     /*public String obtenerUsuarios(){
         String query= "SELECT * FROM sys."+TABLE_USUARIOS;
         String salida="";
