c3bd8b99c604a0c964cc7142008f26edc8d57763
AldairBryan <asalcedoch@unsa.edu.pe>
2022-12-20 21:36:19 -0500

    Se eliminaron clases innecesarias

--- app/src/main/java/com/example/ecoclub/database/DataBaseHelper.java
+++ app/src/main/java/com/example/ecoclub/database/DataBaseHelper.java
@@ -1,12 +1,11 @@
 package com.example.ecoclub.database;
 
-import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
-
-public class DBHelper extends SQLiteOpenHelper {
+public class DataBaseHelper {
     private static final int DB_VERSION = 1;
     private static final String DB_NAME = "database-nuna.db";
+    public  static final String url="jdbc:mysql://database-nuna.c1gztg4ki2uu.sa-east-1.rds.amazonaws.com";
+
+    public static final String username="admin", password="XiQ5nzUrRUQ8fKc";
 
     public static final String TABLE_USUARIOS = "Usuarios";
     public static final String TABLE_COMUNIDADES = "Comunidades";
@@ -16,20 +15,7 @@ public class DBHelper extends SQLiteOpenHelper {
     public static final String TABLE_PARTICIPANTES_ACTIVIDADES = "Participantes_actividades";
     public static final String TABLE_ACTIVIDADES = "Actividades";
 
-    public DBHelper(Context context) {
-        super(context, DB_NAME, null, DB_VERSION);
-    }
-
-    @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        String sql = "";
-        sqLiteDatabase.execSQL(sql);
-
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        sqLiteDatabase.execSQL("" );
-        onCreate(sqLiteDatabase);
+    public void testConnection(){
+        
     }
 }
